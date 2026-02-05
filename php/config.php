709bcc41c962b046b7b06fe4b61458fc9cb10771
Drevyar <kraivich.sa@ku.th>
2026-02-05 16:58:35 +0700

    pass

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$user = "std6730202050";
-$pass = "Pz!4jv9Y";
-$db   = "it_std6730202050";
+$user = "std6730202050"; //ชื่อ username ที่จารให้
+$pass = ""; //รหัสที่จากให้
+$db   = "it_std6730202050"; //ชื่อ database
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
